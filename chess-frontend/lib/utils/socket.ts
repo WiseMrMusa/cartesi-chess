6b992af38518ef85244667c47ae3aa59cfa961b7
WiseMrMusa <abdulkareemusa01@gmail.com>
2024-02-01 20:50:50 +0100

    frontend: (bug_fix) removed some unused functions

--- chess-frontend/lib/utils/socket.ts
+++ chess-frontend/lib/utils/socket.ts
@@ -12,20 +12,11 @@ const io = new Server(server, {
 
 io.on('connection', (socket) => {
 
-    console.log("Hello")
+  console.log("Hello")
 
   socket.on('join', () => {
     socket.broadcast.emit('opponentMove', {  from: 'd2' , to: 'd4' })
     console.log("Opponent Makes Move")
-  })
-
-  socket.on('canvas-state', (state) => {
-    console.log('received canvas state')
-    socket.broadcast.emit('canvas-state-from-server', state)
-  })
-
-  socket.on('draw-line', ({ prevPoint, currentPoint, color }: DrawLine) => {
-    socket.broadcast.emit('draw-line', { prevPoint, currentPoint, color })
   })
 
   socket.on('clear', () => io.emit('clear'))
